9db978dc3d9b3f9f0753131da683b4ede13d2a3f
Abdullah khan <akhan283785@gmail.com>
2024-12-19 10:05:11 +0530

    try 1

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function createTable() {
-    //Write your code here
-  
+  //Write your code here
+
+  let table = document.querySelector("#myTable");
+  let tbody = document.createElement("tbody");
+  let rn = prompt("Input number of rows");
+  let cn = prompt("Input number of columns");
+
+  for (let index = 0; index < rn; index++) {
+    let row = tbody.insertRow();
+    for (let j = 0; j < cn; j++) {
+      let td = row.insertCell();
+      td.innerHTML = `Row-${index} Column-${j}`;
+    }
+  }
+  table.append(tbody);
 }
